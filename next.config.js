37a1420beae587941c70380bcda4fdb0891703ab
emirkutlugun77 <57583899+emirkutlugun77@users.noreply.github.com>
2025-02-09 11:27:49 +0300

    gh-pages

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   // Remove trailingSlash and standalone output
   output: 'export',
   // Add basePath if you're not using a custom domain
-  // basePath: '/handenaz',
+  basePath: '/handenaz',
   distDir: 'dist',
   // Add this to ensure proper routing
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
